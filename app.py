7b34b1f2c56084028ef30dbd7ede95e542bf2c3a
Uponika <uponika@gmail.com>
2024-01-15 21:54:16 -0500

    Modified application file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 ## QA Chatbot
+
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
 import os
@@ -11,7 +12,6 @@ import streamlit as st
 
 def get_openai_response(question):
     llm = OpenAI(openai_api_key = os.getenv('OPENAI_API_KEY'), model_name = 'gpt-3.5-turbo-instruct', temperature= 0.6)
-    #llm = OpenAI(model_name = 'gpt-3.5-turbo-instruct', temperature= 0.6)
     response = llm(question)
     return response
 
